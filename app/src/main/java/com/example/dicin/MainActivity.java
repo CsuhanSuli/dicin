d466758bd12c23c2b3f289ecc64d6d080dc72077
Ppet456 <ps.gila456@gmail.com>
2025-12-01 12:28:26 +0100

    rumble

--- app/src/main/java/com/example/dicin/MainActivity.java
+++ app/src/main/java/com/example/dicin/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.dicin;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView dice2 = findViewById(R.id.dice2);
         ImageView dice3 = findViewById(R.id.dice3);
         ImageView[] dices = {dice1,dice2,dice3};
+        Vibrator vibrator = this.getSystemService(Vibrator.class);
         int speed = 50;
         int[] dicesImg = {R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
         int[] soundLinks = {R.raw.horse1,R.raw.horse2,R.raw.horse3,R.raw.horse4,R.raw.horse5};
@@ -48,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this,soundLinks[(int)(Math.random()*5)]);
                 mediaPlayer.setVolume(50,50);
                 mediaPlayer.start();
+                vibrator.vibrate(VibrationEffect.createOneShot(300,200));
             }
         });
         Button reset = findViewById(R.id.reset);
